3198fbd8420db7fb5c1c669e9716d9c4035f0e84
B Sathvika <137168758+bsathvika11@users.noreply.github.com>
2023-07-18 14:28:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ let arr = Array.from(buttons);
 arr.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-      string = eval(string);
+      string = eval(string); //evaluates the expression present on display
       input.value = string;
     } else if (e.target.innerHTML == "AC") {
-      string = "";
+      string = ""; //clears the entire values
       input.value = string;
     } else if (e.target.innerHTML == "Del") {
-      string = string.toString().slice(0, -1);
+      string = string.toString().slice(0, -1); //removes the last digit
       input.value = string;
     } else {
       string += e.target.innerHTML;
@@ -35,7 +35,13 @@ function tan() {
   string = Math.tan(string);
   input.value = string;
 }
+
 function sqrt() {
   string = Math.sqrt(string, 2);
   input.value = string;
 }
+/* other way:
+function pow() {
+  string = Math.pow(string, 0.5);
+  input.value = string;
+}*/
